9f1fe26926b2f2dae3a3b6d6b44d78edd07aca5a
VoxVuiVe <vovuive2842003@gmail.com>
2023-10-10 14:19:27 +0700

    start project LearnifyApp

---
+++ src/test/java/com/project/shopapp/LearnifyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.shopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnifyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
