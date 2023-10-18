dc6be35a456b4647c3a798fb0f8702c4192979ad
CuongDV45 <cuongdang10001@.gmailcom>
2023-10-18 20:27:36 +0700

    DONE:Entity Model: Cart, Payment_History, Role

--- src/main/java/com/project/shopapp/models/Cart.java
+++ src/main/java/com/project/shopapp/models/Cart.java
@@ -1,4 +1,43 @@
 package com.project.shopapp.models;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "cart")
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_id")
+    private Long cartId;
+
+    @Column(name = "create_date")
+    private LocalDateTime createDate;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "price")
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", referencedColumnName = "course_id")
+    private Course course;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    private User user;
+
+    @Column(name = "payment_id")
+    private Long paymentId;
 }
--- src/main/java/com/project/shopapp/models/Role.java
+++ src/main/java/com/project/shopapp/models/Role.java
@@ -1,4 +1,22 @@
 package com.project.shopapp.models;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "role")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long roleId;
+
+    @Column(name = "name")
+    private String name;
 }
