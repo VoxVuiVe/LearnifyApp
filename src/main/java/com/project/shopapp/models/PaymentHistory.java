4fbe6a746702be0bf7c7cf04bffcacf7ef795e27
CuongDV45 <cuongdang10001@gmail.com>
2023-10-19 11:44:32 +0700

    DONE: entity models: cart, payment_history, role

--- src/main/java/com/project/shopapp/models/PaymentHistory.java
+++ src/main/java/com/project/shopapp/models/PaymentHistory.java
@@ -1,6 +1,7 @@
 package com.project.shopapp.models;
+import jakarta.persistence.*;
+import lombok.*;
 
-<<<<<<< HEAD
 import java.time.LocalDateTime;
 
 
@@ -11,7 +12,37 @@ import java.time.LocalDateTime;
 @Setter
 @Entity
 @Table(name = "payment_history")
-=======
->>>>>>> parent of dc6be35 (DONE:Entity Model: Cart, Payment_History, Role)
 public class PaymentHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "payment_history_id")
+    private Long paymentId;
+
+    @Column(name = "total_money")
+    private Double totalMoney;
+
+    @Column(name = "number_of_course")
+    private Integer numberOfCourse;
+
+    @Column(name = "transaction_status")
+    private String transactionStatus;
+
+    @Column(name = "payment_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime paymentDate;
+
+    @Column(name = "payment_method")
+    private String paymentMethod;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "payment_id", nullable = false)
+    private Payment payment;
 }
