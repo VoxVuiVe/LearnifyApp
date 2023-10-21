91e3b6ef9e3a9d0bc14d86b4dc9e8a57813d22fa
luannv <nvluan.1201@gmail.com>
2023-10-22 00:28:32 +0700

    Done: cmt,favor,rating

--- src/main/java/com/project/shopapp/models/Comment.java
+++ src/main/java/com/project/shopapp/models/Comment.java
@@ -1,4 +1,98 @@
 package com.project.shopapp.models;
 
+import java.util.Date;
+
+import javax.swing.text.AbstractDocument.Content;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter@Setter
+@Entity
+@Table(name = "rating")
+public class Rating {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name ="user_id")
+	private Long userId;
+	
+	@ManyToOne
+	@JoinColumn(name = "course_id")
+	private Course course;
+	
+	@ManyToOne
+	@JoinColumn(name ="content_id")
+	private Content content;
+	
+	@Column(name ="create_data")
+	private Date createDate;
+	
+	@Column(name ="number_of_like_comments")
+	private int number_of_like_comments;
+	
+}
+
+
+
+package com.project.shopapp.models;
+
+import java.util.Date;
+
+import javax.swing.text.AbstractDocument.Content;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter@Setter
+@Entity
+@Table(name = "comment")
 public class Comment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name ="user_id")
+	private Long userId;
+	
+	@ManyToOne
+	@JoinColumn(name = "course_id")
+	private Course course;
+	
+	@ManyToOne
+	@JoinColumn(name = "content_id")
+	private Content content;
+	
+	@Column(name ="create_data")
+	private Date createDate;
+	
+	@Column(name ="number_of_like_comments")
+	private int number_of_like_comments;
+	
+	
+		
 }
--- src/main/java/com/project/shopapp/models/Favourite.java
+++ src/main/java/com/project/shopapp/models/Favourite.java
@@ -1,4 +1,45 @@
 package com.project.shopapp.models;
 
+import java.util.Date;
+
+import javax.swing.text.AbstractDocument.Content;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter@Setter
+@Entity
+@Table(name = "favourite")
 public class Favourite {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name ="favourite_id")
+	private Long favouriteId;
+	
+	@Column(name ="isActive")
+	private Boolean isActive ;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	@ManyToOne
+	@JoinColumn(name = "course_id")
+	private Course course;
+	
+
+	
 }
