04d8000b37d208a90b02f7589bd961fc7f410d70
CuongDV45 <cuongdang10001@gmai.com>
2023-11-03 22:15:02 +0700

    DONE: controller, DTO, Interface

--- src/main/java/com/project/shopapp/models/Course.java
+++ src/main/java/com/project/shopapp/models/Course.java
@@ -17,30 +17,30 @@ import java.time.LocalDateTime;
 public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column (name = "course_id")
+    @Column(name = "course_id")
     private Long courseId;
 
-    @Column (name = "title", columnDefinition = "nvarchar(255)")
+    @Column(name = "title", columnDefinition = "nvarchar(255)")
     private String title;
 
-    @Column (name = "price")
+    @Column(name = "price")
     private double price;
 
-    @Column (name = "teacher", columnDefinition = "nvarchar(50)")
+    @Column(name = "teacher", columnDefinition = "nvarchar(50)")
     private String teacher;
 
-    @Column (name = "start_time")
-    private LocalDateTime start_time;
+    @Column(name = "start_time")
+    private LocalDateTime startTime;
 
-    @Column (name = "end_time")
-    private LocalDateTime end_time;
+    @Column(name = "end_time")
+    private LocalDateTime endTime;
 
-    @Column (name = "enrollment_count")
-    private double enrollment_count;
+    @Column(name = "enrollment_count")
+    private int enrollmentCount;
 
-    @Column (name = "last_update_time")
-    private LocalDateTime last_update_time;
+    @Column(name = "last_update_time")
+    private LocalDateTime lastUpdateTime;
 
-    @Column (name = "image", columnDefinition = "nvarchar(255)")
+    @Column(name = "image", columnDefinition = "nvarchar(255)")
     private String image;
 }
--- src/main/java/com/project/shopapp/models/UserRole.java
+++ src/main/java/com/project/shopapp/models/UserRole.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "userrole")
+@Table(name = "user_role")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
