e097a1183054bda394a0042aad98cf8749224482
CuongDV45 <cuongdang10001@gmail.com>
2023-12-03 16:31:18 +0700

    bổ sung code Section

--- src/main/java/com/project/learnifyapp/service/ISectionService.java
+++ src/main/java/com/project/learnifyapp/service/ISectionService.java
@@ -2,15 +2,21 @@ package com.project.learnifyapp.service;
 
 import com.project.learnifyapp.dtos.CategoryDTO;
 import com.project.learnifyapp.dtos.SectionDTO;
+import com.project.learnifyapp.exceptions.DataNotFoundException;
 import com.project.learnifyapp.models.Category;
 import com.project.learnifyapp.models.Section;
 
 import java.util.List;
 
 public interface ISectionService {
-    Section crateSection(SectionDTO sectionDTO);
-    Section getSectionById(Long id);
-    List<Section> getAllSections();
-    Category updateSection(Long categoryId, CategoryDTO categoryDTO);
-    void delete(Long id);
+    SectionDTO save(SectionDTO sectionDTO) throws DataNotFoundException;
+    SectionDTO getSection(Long id) throws DataNotFoundException;
+    List<SectionDTO> getAllSections();
+
+    SectionDTO update(Long id, SectionDTO sectionDTO) throws DataNotFoundException;
+
+    SectionDTO remove(Long id) throws DataNotFoundException;
+
+
+    boolean exitsById(Long id);
 }
