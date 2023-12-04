3faa0ae4110ab747375bc35270d03f50ce5bd828
cuonglazy <cuong03705@gmail.com>
2023-12-04 08:48:51 +0700

    DONE: Config VNPAY

--- src/main/java/com/project/learnifyapp/configurations/VNPayConfiguration.java
+++ src/main/java/com/project/learnifyapp/configurations/VNPayConfiguration.java
@@ -15,8 +15,8 @@ import java.util.*;
 public class VNPayConfiguration {
     public static String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
     public static String vnp_Returnurl = "/vnpay-payment";
-    public static String vnp_TmnCode = "";
-    public static String vnp_HashSecret = "";
+    public static String vnp_TmnCode = "SE43QYX7";
+    public static String vnp_HashSecret = "YNDEEGLZNPSKNZIADIXJREZEOLIDSEJQ";
     public static String vnp_apiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
 
     public static String md5(String message) {
