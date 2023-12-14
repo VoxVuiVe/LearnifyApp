11367f88df7376c2dd646ab37404696f547cc1b3
TruongNS5 <TruongNS5@fpt.com>
2023-12-14 23:02:52 +0700

    Done: Page Course

--- src/main/java/com/project/learnifyapp/controllers/CourseController.java
+++ src/main/java/com/project/learnifyapp/controllers/CourseController.java
@@ -7,11 +7,15 @@ import com.project.learnifyapp.service.impl.CourseService;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 @RestController
@@ -62,6 +66,21 @@ public class CourseController {
     public ResponseEntity<List<CourseDTO>> getAllCourse(){
         List<CourseDTO> courseDTOS = courseService.findAll();
         return ResponseEntity.ok(courseDTOS);
+    }
+
+    @GetMapping("/pages")
+    public ResponseEntity<Map<String, Object>> getCoursePage(
+            @RequestParam(name = "keyword",required = false,defaultValue = "") String keyword,
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "10") int size ){
+        PageRequest pageRequest = PageRequest.of(page,size);
+        Page<CourseDTO> courses = courseService.findAllPage(keyword,pageRequest);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("courses",courses.getContent());
+        response.put("totalPages",courses.getTotalPages());
+
+        return new ResponseEntity<>(response,HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
--- src/main/java/com/project/learnifyapp/service/ICourseService.java
+++ src/main/java/com/project/learnifyapp/service/ICourseService.java
@@ -1,6 +1,8 @@
 package com.project.learnifyapp.service;
 
 import com.project.learnifyapp.dtos.CourseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -10,6 +12,9 @@ public interface ICourseService {
 
     @Transactional(readOnly = true)
     List<CourseDTO> findAll();
+
+    Page<CourseDTO> findAllPage(String keyword, PageRequest pageRequest);
+
 
     @Transactional(readOnly = true)
     CourseDTO findOne(Long id);
