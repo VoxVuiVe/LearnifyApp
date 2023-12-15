aaee672cf83e1915961d6eb4cfdede4a5d658bef
cuonglazy <cuong03705@gmail.com>
2023-12-15 10:03:49 +0700

    in-progress

--- src/main/java/com/project/learnifyapp/service/mapper/CourseMapper.java
+++ src/main/java/com/project/learnifyapp/service/mapper/CourseMapper.java
@@ -5,7 +5,7 @@ import com.project.learnifyapp.models.Course;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring", uses = {CategoryMapper.class, UserMapper.class, SectionMapper.class})
+@Mapper(componentModel = "spring", uses = {CategoryMapper.class, UserMapper.class})
 public interface CourseMapper extends EntityMapper<CourseDTO, Course>{
 
     @Override
